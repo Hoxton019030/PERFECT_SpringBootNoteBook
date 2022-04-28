7757415daf4ffd2de3836ab959cf1ab48b8bc0ab
Hoxton <za546321@yahoo.com.tw>
2022-04-28 14:30:36 +0800

    2022/4/28

---
+++ src/main/java/com/jerryboot/springbootdemo/controller/CustomerController.java
@@ -0,0 +1,137 @@
+package com.jerryboot.springbootdemo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jerryboot.springbootdemo.model.Customer;
+import com.jerryboot.springbootdemo.model.CustomerDao;
+
+@RestController // 表示本Class回傳的都是JSON格式
+public class CustomerController {
+	@Autowired
+	private CustomerDao dao;
+
+	@PostMapping("customer/insert")
+	public Customer insertCustomer() {
+		Customer cus = new Customer();
+		cus.setName("館長");
+		cus.setLevel(3);
+		Customer resCus = dao.save(cus);
+		return resCus;
+	}
+
+	@PostMapping("customer/insert2")
+
+	public Customer insertCustomer2(@RequestBody Customer cus) {
+		// @RequestBody:傳到Controller內的
+		// @Respo nseBody:傳到Contrller外的
+		Customer resCus = dao.save(cus);
+		return resCus;
+	}
+
+	@PostMapping("customer/insert3")
+	public List<Customer> insertCustomer3(@RequestBody List<Customer> cus) {
+		// @RequestBody:傳到Controller內的
+		// @Respo nseBody:傳到Contrller外的
+		List<Customer> resCus = dao.saveAll(cus);
+		return resCus;
+	}
+
+	@GetMapping("customer/get")
+	public Customer getCustomerById(@RequestParam("id") Integer id) {
+		// http://localhost:8080/myapp/customer/get?id=
+		Optional<Customer> optional = dao.findById(id);
+
+		if (optional.isPresent()) {
+			Customer resCus = optional.get();
+			return resCus;
+		}
+
+		return null;
+	}
+
+	@GetMapping("customer/get2/{id}") // 路徑變數的寫法
+	public Customer getCustomerById2(@PathVariable Integer id) {
+		// http://localhost:8080/myapp/customer/get?id=
+		Optional<Customer> optional = dao.findById(id);
+
+		if (optional.isPresent()) {
+			Customer resCus = optional.get();
+			return resCus;
+		}
+
+		return null;
+	}
+
+	@GetMapping("customer/getall")
+	public List<Customer> getAllCustomer() {
+		// http://localhost:8080/myapp/customer/get?id=
+		List<Customer> customerList = dao.findAll();
+
+		if (customerList.isEmpty() != true) {
+			return customerList;
+		}
+
+		return null;
+	}
+
+	@GetMapping("customer/delete/{id}") // 路徑變數寫法
+	public void deleteCustomer(@PathVariable Integer id) {
+		dao.deleteById(id);
+	}
+
+	@GetMapping("customer/delete2")
+	public void deleteCustomer2(@RequestParam Integer id) {
+		Customer cus = dao.getById(id);
+		dao.delete(cus);
+	}
+
+	@GetMapping("customer/page/{pageNumber}")
+	public List<Customer> findByPage(@PathVariable Integer pageNumber) {
+		PageRequest pgb = PageRequest.of(pageNumber - 1, 3, Sort.Direction.ASC, "id");
+		// (頁數,一頁幾筆資料,排序方式,排序依照)
+		Page<Customer> page = dao.findAll(pgb);
+		List<Customer> list = page.getContent();
+		return list;
+
+	}
+
+	@GetMapping("customer/findbyName")
+	public List<Customer> findByName(@RequestParam("name") String name) {
+		List<Customer> list = dao.findCustomerByName(name);
+		return list;
+	}
+
+	@GetMapping("customer/findbyName2")
+	public List<Customer> findByName2(@RequestParam("name") String name) {
+		List<Customer> list = dao.findCustomerByName2(name);
+		return list;
+	}
+	@GetMapping("customer/findbyName3")
+	public List<Customer> findByName3(@RequestParam("name") String name,@RequestParam("level") Integer level) {
+		List<Customer> list = dao.findCustomerByName3(name,level); 
+		return list;
+	}
+	
+	@GetMapping("customer/delete")
+	public boolean deleteCustomer(@RequestParam("name")String name) {
+		List<Customer> findByName2 = findByName2(name);
+		
+		if(findByName2.isEmpty()==false) {
+			dao.deleteCustomerByName(name);
+			return true;
+		}
+		return false;
+	}
+}
---
+++ src/main/java/com/jerryboot/springbootdemo/model/CustomerDao.java
@@ -0,0 +1,31 @@
+package com.jerryboot.springbootdemo.model;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository // DAO都要寫
+public interface CustomerDao extends JpaRepository<Customer, Integer> {
+
+	//HQL語法
+	@Query(value="from Customer where name =:name0") // 指Customer的名字name=:name0要跟@Param("name0")對應到
+	public List<Customer> findCustomerByName(@Param("name0") String name);
+	
+	//原生SQL語法，要加nativeQuery=true來啟用原生SQL
+	@Query(value="SELECT * FROM customer WHERE name=:name1",nativeQuery = true)
+	public List<Customer> findCustomerByName2(@Param("name1") String name);
+	
+	@Query(value="SELECT * FROM customer WHERE name=:name2 AND level= :level",nativeQuery = true)
+	public List<Customer> findCustomerByName3(@Param("name2") String name,@Param("level") Integer level);
+	
+//	?1=第一個參數
+	@Query(value="DELETE FROM customer WHERE name=?1",nativeQuery = true)
+	@Transactional
+	@Modifying //沒有Service Annotaion標註此項的話，要再修改資料的地方標註
+	public void deleteCustomerByName(String name);
+}
